d304768ca8fe4f62853a5546cea5d0af5bb686aa
Pedro Costa <pedrohsc100@gmail.com>
2023-03-06 00:15:09 -0300

    fix: consertados DER, DOC e requeriments

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -21,7 +21,7 @@ from drf_spectacular.views import (
 )
 
 urlpatterns = [
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularAPIView.as_view(), name='schema'),
     path('api/docs/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
